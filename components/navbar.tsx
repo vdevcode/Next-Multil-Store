733945855fad7c18bc519540dc87d6001c99f0ea
vdevcode <thanhvumaidao1234567@gmail.com>
2024-05-16 17:48:13 +0700

    feat: add settings navabr

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,13 +2,17 @@ import { UserButton } from "@clerk/nextjs";
 import { MainNav } from "./main-nav";
 
 export const Navbar = () => {
-  return <div className="border-b">
-    <div className="flex h-16 items-center px-4 justify-between">
-        <p>This is store switcher</p>
-        <MainNav />
-        <div className="">
-            <UserButton />
+  return (
+    <div className="border-b">
+      <div className="flex h-16 items-center px-4 justify-between">
+        <div className="flex items-center gap-4">
+          <p>This is store switcher</p>
+          <MainNav />
         </div>
+        <div className="">
+          <UserButton />
+        </div>
+      </div>
     </div>
-  </div>;
+  );
 };

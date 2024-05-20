16725796c42cf08acfd508214dd156704d522338
vdevcode <thanhvumaidao1234567@gmail.com>
2024-05-20 08:35:49 +0700

    refactor: api delete billboards

--- app/api/[storeId]/billboards/[billboardId]/route.ts
+++ app/api/[storeId]/billboards/[billboardId]/route.ts
@@ -1,7 +1,7 @@
 import { db } from "@/lib/firebase";
 import { BillBoards } from "@/types-db";
 import { auth } from "@clerk/nextjs/server";
-import { doc, getDoc, updateDoc } from "firebase/firestore";
+import { deleteDoc, doc, getDoc, updateDoc } from "firebase/firestore";
 import { NextResponse } from "next/server";
 
 export const PATCH = async (
@@ -75,46 +75,58 @@ export const DELETE = async (
   req: Request,
   { params }: { params: { storeId: string; billboardId: string } }
 ) => {
-  const { userId } = auth();
-  const body = await req.json();
+  try {
+    const { userId } = auth();
+    const body = await req.json();
 
-  if (!userId) {
-    return new NextResponse("Un-authorize", { status: 400 });
-  }
-  const { label, imageUrl } = body;
-  if (!label) {
-    return new NextResponse("Billboard name is missing!!! ", { status: 400 });
-  }
-
-  if (!imageUrl) {
-    return new NextResponse("Image billboards is missing!!!", {
-      status: 400,
-    });
-  }
-
-  if (!params.storeId) {
-    return new NextResponse("StoreId is missing!!!", { status: 400 });
-  }
-
-  if (!params.billboardId) {
-    return new NextResponse("Billboard is missing", { status: 400 });
-  }
-
-  const store = await getDoc(doc(db, "stores", params.storeId));
-  if (store.exists()) {
-    let storeData = store.data();
-    if (storeData?.userId !== userId) {
-      return new NextResponse("Un-authorize access", { status: 500 });
+    if (!userId) {
+      return new NextResponse("Un-authorize", { status: 400 });
     }
-  }
+    const { label, imageUrl } = body;
+    if (!label) {
+      return new NextResponse("Billboard name is missing!!! ", { status: 400 });
+    }
 
-  const billboardRef = await getDoc(
-    doc(db, "stores", params.storeId, "billboards", params.billboardId)
-  );
+    if (!imageUrl) {
+      return new NextResponse("Image billboards is missing!!!", {
+        status: 400,
+      });
+    }
 
-  if (billboardRef.exists()) {
-    
-  } else {
-    return new NextResponse("Billboard not found", { status: 404 });
+    if (!params.storeId) {
+      return new NextResponse("StoreId is missing!!!", { status: 400 });
+    }
+
+    if (!params.billboardId) {
+      return new NextResponse("Billboard is missing", { status: 400 });
+    }
+
+    const store = await getDoc(doc(db, "stores", params.storeId));
+    if (store.exists()) {
+      let storeData = store.data();
+      if (storeData?.userId !== userId) {
+        return new NextResponse("Un-authorize access", { status: 500 });
+      }
+    }
+
+    const billboardRef = doc(
+      db,
+      "stores",
+      params.storeId,
+      "billboards",
+      params.billboardId
+    );
+
+    await deleteDoc(billboardRef);
+    const billboard = (
+      await getDoc(
+        doc(db, "store", params.storeId, "billboards", params.billboardId)
+      )
+    ).data() as BillBoards;
+
+    return NextResponse.json({ billboard });
+  } catch (error) {
+    console.log(`Stored patch: ${error}`);
+    return new NextResponse("server error", { status: 500 });
   }
 };

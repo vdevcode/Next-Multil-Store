ce99a5abf9c5eb004d460d2021f91b9dc7c69760
vdevcode <thanhvumaidao1234567@gmail.com>
2024-05-20 08:48:13 +0700

    chore: add table shadcn

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,13 +1,20 @@
-import { BillBoardClient } from "./_components/client"
+import { collection, doc, getDocs } from "firebase/firestore";
+import { BillBoardClient } from "./_components/client";
+import { db } from "@/lib/firebase";
+import { BillBoards } from "@/types-db";
 
-const BillBoards = ({params}: {params: {storeId: string }}) => {
-    return (
-        <div className="flex-col">
-            <div className="flex-1 space-x-4 p-8 pt-6">
-                <BillBoardClient />
-            </div>
-        </div>
-    )
-}
+const BillBoardsPage = async ({ params }: { params: { storeId: string } }) => {
+  const billboardData = (
+    await getDocs(collection(doc(db, "stores", params.storeId), "billboards"))
+  ).docs.map((doc) => doc.data() as BillBoards);
 
-export default BillBoards
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-x-4 p-8 pt-6">
+        <BillBoardClient />
+      </div>
+    </div>
+  );
+};
+
+export default BillBoardsPage;

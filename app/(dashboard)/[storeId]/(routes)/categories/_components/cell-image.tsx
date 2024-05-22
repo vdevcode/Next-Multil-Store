deb5354b2022f3fcedd8fac0d523c30c693e636e
vdevcode <thanhvumaidao1234567@gmail.com>
2024-05-22 11:02:37 +0700

    feat: add caterories store

---
+++ app/(dashboard)/[storeId]/(routes)/categories/_components/cell-image.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import Image from "next/image"
+
+
+interface CellImageProps{
+    imageUrl: string
+}
+
+export const CellImage = ({imageUrl}: CellImageProps) => {
+    return (
+        <div className="w-32 min-h-16 overflow-hidden min-w-32 relative rounded-sm shadow-md cursor-pointer">
+            <Image fill alt="Billboard image" className="object-cover" src={imageUrl} />
+        </div>
+    )
+}
+
+
